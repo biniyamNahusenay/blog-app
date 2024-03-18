15f8eb19850620db0d1043593dcb2400130b652e
biniyamNahusenay <bininahu12@gmail.com>
2024-03-18 05:48:56 -0700

    configuring routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
 
 export default function App() {
   return (
-    <h1 className="text-red-800 font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/about" element={<About/>}/>
+         <Route path="/signup" element={<Signup/>}/>
+         <Route path="/signin" element={<Signin/>}/>
+         <Route path="/projects" element={<Projects/>}/>
+         <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
