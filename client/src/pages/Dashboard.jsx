1f630d7c40a313a3519abd2303dca70a4b4b4753
biniyamNahusenay <bininahu12@gmail.com>
2024-03-25 01:53:52 -0700

    complete profile page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ export default function Dashboard() {
   },[location.search])
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className='min-h-screen flex flex-col md:flex-row md:gap-80'>
        <div className='md:w-56'>
         <DashSidebar/>
        </div>
