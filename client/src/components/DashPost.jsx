43879a9e5b83a282dafa12283c4ddf8d34b65413
biniyamNahusenay <bininahu12@gmail.com>
2024-04-11 09:19:57 -0700

    add delete post functionality in the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
-import {Table} from 'flowbite-react'
+import {Modal, Table,Button} from 'flowbite-react'
 import {Link} from 'react-router-dom'
+import {HiOutlineExclamationCircle} from 'react-icons/hi'
 
 export default function DashPost() {
 const {currentUser} = useSelector((state)=>state.user)
 const [userPosts,setUserPosts] = useState([])
 const [showMoree, setShowMoree] = useState(true);
+const [showModal,setShowModal] = useState(false)
+const [postIdToDelete,setPostIdToDelete] = useState('')
 
 useEffect(() => {
   const fetchPosts = async () => {
@@ -43,6 +46,28 @@ const handleShowMore = async () => {
     console.log(error.message);
   }
 };
+
+const handleDeletePost = async ()=>{
+  setShowModal(false)
+  try {
+     const res = await fetch(
+      `/api/post/deletePost/${postIdToDelete}/${currentUser._id}`,
+      {
+        method:'DELETE',
+      }
+     );
+     const data = await res.json()
+     if(!res.ok){
+      console.log(data.message)
+     }else{
+      setUserPosts((prev)=>{
+        prev.filter((post)=>post._id !== postIdToDelete)
+      })
+     }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
   return (
     <div style={{marginLeft:"-25px"}} className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -87,6 +112,10 @@ const handleShowMore = async () => {
                   <Table.Cell>
                     <span
                       className='font-medium text-red-500 hover:underline cursor-pointer'
+                      onClick={()=>{
+                        setShowModal(true)
+                        setPostIdToDelete(post._id)
+                      }}
                     >
                       Delete
                     </span>
@@ -116,6 +145,19 @@ const handleShowMore = async () => {
           <p>You Have No Posts Yet</p>
         )
       }
+         <Modal show={showModal} onClose={()=>setShowModal(false)} popup size='md'>
+         <Modal.Header/>
+         <Modal.Body>
+            <div className="text-center">
+               <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+            </div>
+            <h3 className='mb-5 text-lg text-gary-500 dark:text-gray-400'>Are you sure you want delete this post?</h3>
+         </Modal.Body>
+         <div className="flex justify-center gap-4 mb-3">
+           <Button color='failure' onClick={handleDeletePost}>Yes,I'm sure</Button>
+           <Button color='gray' onClick={()=>setShowModal(false)}>No,cancel</Button>
+         </div>
+      </Modal>
     </div>
   )
 }
