b010732da553c271a976a98276cc30cbb3851891
biniyamNahusenay <bininahu12@gmail.com>
2024-04-16 00:26:57 -0700

    show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -45,6 +45,17 @@ export default function DashSidebar() {
               </Sidebar.Item>
             </Link>
           )}
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=users'>
+              <Sidebar.Item
+                 active={tab === 'users'}
+                 icon={HiOutlineUserGroup}
+                 as='div'
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
+          )}
            <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
              Sign out
            </Sidebar.Item>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar'
 import DashProfile from '../components/DashProfile'
 import DashPost from '../components/DashPost'
+import DashUsers from '../components/DashUsers'
 
 export default function Dashboard() {
   const location = useLocation()
@@ -24,6 +25,7 @@ export default function Dashboard() {
        <div>
         {tab ==='profile' && <DashProfile/>}
         {tab ==='posts' && <DashPost/>}
+        {tab === 'users' && <DashUsers/>}
        </div>
     </div>
   )
